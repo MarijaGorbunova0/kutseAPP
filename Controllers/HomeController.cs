b9e0db517c5f862805092abf8a9363105adb742a
David-Lennuk <david.nikolajev07@gmail.com>
2025-01-10 08:46:17 +0200

    work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,24 +1,60 @@
-﻿using System;
+﻿using Kutse_App.Models;
+using Peokutse.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
-namespace Kutse_App.Controllers
+namespace Peokutse.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
+            ViewBag.Message = "Ootan sind minu poele! Palun tule!!!";
             int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 10 ? "Tere hommilust!" : "Tere päevast!";
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
             return View();
         }
         [HttpGet]
         public ViewResult Ankeet()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult Ankeet(Guest guest)
+        {
+            E_Mail(guest);
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public void E_Mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "david.nikolajev07@gmail.com";
+                WebMail.Password = "azni mflj nrsq humz";
+                WebMail.From = "david.nikolajev07@gmail.com";
+                WebMail.Send("david.nikolajev07@gmail.com", "Vastus kutsele", guest.Name + " Vastus " + ((guest.WillAttend ?? false) ?
+                    "tuleb poele " : "ei tule poele"));
+                ViewBag.Message = "Kiri on saatnud!";
+            }
+            catch (Exception exception)
+            {
+                ViewBag.Message = "Mul on kuhju! Ei saa kirja saada!!!";
+            }
         }
     }
 }
--- Models/Guest.cs
+++ Models/Guest.cs
@@ -1,20 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
 
-namespace Kutse_App.Models
+namespace Peokutse.Models
 {
     public class Guest
     {
-        [Required(ErrorMessage = "Sisesta nimi")]
-        public int Name { get; set; }
-        [Required(ErrorMessage = "Sisesta email")]
-        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Valesti sisestatud email")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Sisesta telefoni number")]
-        [RegularExpression(@"\+372.+", ErrorMessage = "Numbri alguses peal olema +372")]
-        public string Phone { get; set; }
-        [Required(ErrorMessage = "Sisesta oma valik")]
-        
-        public bool? WillAttend { get; set; }
+        [Required(ErrorMessage = "Sisestage nimi!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Sisestage email!")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Valesti sisestatud email!")]
 
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Sisestage telefoni number!")]
+        [RegularExpression(@"\+372.+", ErrorMessage = "Numbri alguses peal olema +372")]
+
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Sisestage oma valik!")]
+
+        public bool? WillAttend { get; set; }
     }
 }
